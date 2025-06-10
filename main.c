82682d4fc910721f021e061c023c6931a0931d43
Nedal Abu Haltam <nedalhaltam1@gmail.com>
2025-06-10 20:50:46 +0300

    cooler

--- main.c
+++ main.c
@@ -10,7 +10,7 @@
 
 #include "raylib/include/raylib.h"
 
-int K = 8;
+int NumberOfColors = 0;
 #define MAX_ITER 30
 
 typedef struct
@@ -34,7 +34,7 @@ int ClosestCluster(Colorf color, Colorf *centroids) {
     int index = 0;
     float min_dist = color_distance(color, centroids[0]);
 
-    for (int i = 1; i < K; ++i) {
+    for (int i = 1; i < NumberOfColors; ++i) {
         float dist = color_distance(color, centroids[i]);
         if (dist < min_dist) {
             min_dist = dist;
@@ -52,9 +52,9 @@ void Cluster(Colorf* Centroids, Point* Points, int count)
     }
 
     // Recompute Centroids
-    int* CentroidCount = (int*)malloc(sizeof(int) * K);
-    Colorf* NewCentroids = (Colorf*)malloc(sizeof(Colorf) * K);
-    for (int i = 0; i < K; i++)
+    int* CentroidCount = (int*)malloc(sizeof(int) * NumberOfColors);
+    Colorf* NewCentroids = (Colorf*)malloc(sizeof(Colorf) * NumberOfColors);
+    for (int i = 0; i < NumberOfColors; i++)
     {
         CentroidCount[i] = 0;
         NewCentroids[i] = (Colorf){0};
@@ -68,7 +68,7 @@ void Cluster(Colorf* Centroids, Point* Points, int count)
         CentroidCount[cluster]++;
     }
 
-    for (int i = 0; i < K; ++i) {
+    for (int i = 0; i < NumberOfColors; ++i) {
         if (CentroidCount[i] > 0) {
             Centroids[i].r = NewCentroids[i].r / CentroidCount[i];
             Centroids[i].g = NewCentroids[i].g / CentroidCount[i];
@@ -77,6 +77,31 @@ void Cluster(Colorf* Centroids, Point* Points, int count)
     }    
     free(CentroidCount);
     free(NewCentroids);
+}
+
+int count_unique_colors(uint32_t *image, int count) {
+    // Use a simple hash table for color uniqueness
+    // For small images, a fixed-size table is fine
+    #define HASH_SIZE 65536
+    int unique = 0;
+    uint32_t *table = (uint32_t*)calloc(HASH_SIZE, sizeof(uint32_t));
+    char *used = (char*)calloc(HASH_SIZE, sizeof(char));
+    for (int i = 0; i < count; ++i) {
+        uint32_t color = image[i] & 0x00FFFFFF; // ignore alpha
+        uint32_t hash = ((color >> 16) ^ (color >> 8) ^ color) % HASH_SIZE;
+        // Linear probing
+        while (used[hash] && table[hash] != color) {
+            hash = (hash + 1) % HASH_SIZE;
+        }
+        if (!used[hash]) {
+            used[hash] = 1;
+            table[hash] = color;
+            unique++;
+        }
+    }
+    free(table);
+    free(used);
+    return unique;
 }
 
 void Initialize(Point* Points, Colorf* centroids, uint32_t *image, int count)
@@ -89,7 +114,7 @@ void Initialize(Point* Points, Colorf* centroids, uint32_t *image, int count)
     }
     
     // Initialize centroids randomly
-    for (int i = 0; i < K; ++i) {
+    for (int i = 0; i < NumberOfColors; ++i) {
         int rand_index = rand() % count;
         centroids[i] = Points[rand_index].color;
     }
@@ -98,8 +123,8 @@ void Initialize(Point* Points, Colorf* centroids, uint32_t *image, int count)
 void kmeans_quantization(uint32_t *image, int Width, int Height) {
     int Pixels = Width * Height;
     Point* Points = (Point*)malloc(sizeof(Point) * Pixels);
-    Colorf* centroids = (Colorf*)malloc(sizeof(Colorf)*K);
-    for (int i = 0; i < K; i++)
+    Colorf* centroids = (Colorf*)malloc(sizeof(Colorf)*NumberOfColors);
+    for (int i = 0; i < NumberOfColors; i++)
     {
         centroids[i] = (Colorf){0};
     }
@@ -120,7 +145,7 @@ void kmeans_quantization(uint32_t *image, int Width, int Height) {
 
 int w = 800;
 int h = 600;
-float scale = 0.5f;
+float scale = 0.4f;
 #define BACKGROUND_COLOR ((Color){ .r = 0x20, .g = 0x20, .b = 0x20, .a = 0xFF})
 
 Image meLoadImage(const char* FilePath)
@@ -149,12 +174,22 @@ int main(int argc, char *argv[]) {
     SetTargetFPS(60);
     Image image = meLoadImage(ImageFilePath);
     Texture2D texture = LoadTextureFromImage(image);
+    // in general it takes some time...
+    // NumberOfColors = count_unique_colors((uint32_t*)image.data, image.width * image.height);
+    NumberOfColors = 1;
+    float thick = 4.0f;
+    int fontsize = 20;
+    Rectangle dest = (Rectangle) { .x = w / 2 - scale*texture.width / 2, .y = h / 2 - scale*texture.height / 2, .width = scale*texture.width, .height = scale*texture.height};
+    Rectangle lines = (Rectangle) { .x = dest.x - thick, .y = dest.y - thick, .width = dest.width + 2 * thick, .height = dest.height + 2 * thick};
+    bool QUANTIZE = false;
+    int NumberEntered = 0;
     while (!WindowShouldClose())
     {
         w = GetScreenWidth();
         h = GetScreenHeight();
-        if (IsKeyPressed(KEY_Q))
+        if (QUANTIZE)
         {
+            QUANTIZE = false;
             UnloadImage(image);
             image = meLoadImage(ImageFilePath);
             kmeans_quantization((uint32_t*)image.data, image.width, image.height);
@@ -168,13 +203,47 @@ int main(int argc, char *argv[]) {
                 fprintf(stderr, "Could not save image\n");
             }
         }
-        if (IsKeyPressed(KEY_RIGHT))
-            K++;
-        if (IsKeyPressed(KEY_LEFT))
-            K--;
+        int key = GetKeyPressed();
+        if ((KEY_ZERO <= key && key <= KEY_NINE) || (KEY_KP_0 <= key && key <= KEY_KP_9))
+        {
+            if (KEY_ZERO <= key && key <= KEY_NINE)
+                key -= KEY_ZERO;
+            else if (KEY_KP_0 <= key && key <= KEY_KP_9)
+                key -= KEY_KP_0;
+            NumberEntered *= 10;
+            NumberEntered += key;
+        }
+        if (IsKeyPressed(KEY_ENTER) || IsKeyPressed(KEY_KP_ENTER))
+        {
+            NumberOfColors = NumberEntered;
+            NumberEntered = 0;
+        }
+        if (IsKeyPressed(KEY_Q))
+            QUANTIZE = true;
+
         BeginDrawing();
         ClearBackground(BACKGROUND_COLOR);
-        DrawTexturePro(texture, (Rectangle) { .x = 0, .y = 0, .width = texture.width, .height = texture.height}, (Rectangle) { .x = w / 2 - scale*texture.width / 2, .y = h / 2 - scale*texture.height / 2, .width = scale*texture.width, .height = scale*texture.height}, (Vector2) {.x = 0, .y = 0}, 0, WHITE);
+        
+        DrawRectangleLinesEx(lines, thick / 4, RAYWHITE);
+        DrawTexturePro(texture, (Rectangle) { .x = 0, .y = 0, .width = texture.width, .height = texture.height}, dest, (Vector2) {.x = 0, .y = 0}, 0, WHITE);
+
+        const char* ColorCountText = TextFormat("Current Number of Colors: %d", NumberOfColors);
+        int ColorCountTextWidth = MeasureText(ColorCountText, fontsize);
+        DrawText(ColorCountText, w / 2 - ColorCountTextWidth / 2, dest.y + dest.height + fontsize / 2, fontsize, RAYWHITE);
+        
+        if (QUANTIZE)
+        {
+            const char* StatusText = "Quantizing...";
+            int StatusTextWidth = MeasureText(StatusText, fontsize);
+            DrawText(StatusText, w / 2 - StatusTextWidth / 2, dest.y + dest.height + 2 * fontsize, fontsize, RAYWHITE);
+        }
+        else
+        {
+            const char* DoneText = "Done.";
+            int DoneTextWidth = MeasureText(DoneText, fontsize);
+            DrawText(DoneText, w / 2 - DoneTextWidth / 2, dest.y + dest.height + 2 * fontsize, fontsize, RAYWHITE);
+        }
+
         DrawFPS(0, 0);
         EndDrawing();
     }
